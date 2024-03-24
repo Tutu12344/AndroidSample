f43e09d5992bb9271aee71d5be571de590035b3e
Tutu12344 <padde.cross@gmail.com>
2024-03-24 20:34:03 +0900

    listview complete

--- app/src/main/java/com/example/androidsample1/MainActivity.kt
+++ app/src/main/java/com/example/androidsample1/MainActivity.kt
@@ -1,8 +1,19 @@
 package com.example.androidsample1
 
+import android.annotation.SuppressLint
+import android.app.AppComponentFactory
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -12,35 +23,32 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.androidsample1.ui.theme.AndroidSample1Theme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            AndroidSample1Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
+
+        //ListViewオブジェクトを取得。
+        val lvMenu = findViewById<ListView>(R.id.lvMenu)
+        //リストビューに表示するリストデータを作成。
+        var menuList = mutableListOf("から揚げ定食", "ハンバーグ定食", "生姜焼き定食", "ステーキ定食", "野菜炒め定食", "とんかつ定食", "ミンチかつ定食", "チキンカツ定食", "コロッケ定食",  "回鍋肉定食", "麻婆豆腐定食", "青椒肉絲定食", "八宝菜定食", "酢豚定食", "豚の角煮", "焼き鳥", "焼き魚定食", "焼肉定食")
+        //アダプタオブジェクトを生成。
+        val adapter = ArrayAdapter(this@MainActivity, android.R.layout.simple_list_item_1, menuList)
+        //リストビューにアダプタオブジェクトを設定。
+        lvMenu.adapter = adapter
+
+        lvMenu.onItemClickListener = ListItemClickListener()
+
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    private inner class ListItemClickListener:AdapterView.OnItemClickListener{
+        override fun onItemClick(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+            var item = parent.getItemAtPosition(position) as String
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidSample1Theme {
-        Greeting("Android")
+            val show = "あなたが選んだ定食:$item"
+
+            Toast.makeText(this@MainActivity,show,Toast.LENGTH_LONG).show()
+        }
+
     }
 }
