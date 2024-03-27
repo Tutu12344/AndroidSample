bba2cb98c15b6959fc93b3d9eb4936c5772e85a5
Tutu12344 <padde.cross@gmail.com>
2024-03-27 14:33:08 +0900

    media complete

--- app/src/main/java/com/example/androidsample1/MainActivity.kt
+++ app/src/main/java/com/example/androidsample1/MainActivity.kt
@@ -1,136 +1,134 @@
 package com.example.androidsample1
 
 
+import android.media.MediaPlayer
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.Button
+import android.widget.CompoundButton
 import android.widget.EditText
 import android.widget.ListView
 import android.widget.TextView
+import com.google.android.material.switchmaterial.SwitchMaterial
 
 class MainActivity : AppCompatActivity() {
-    /**
-     * 選択されたカクテルの主キーIDを表すプロパティ。
-     */
-    private var _cocktailId = -1
-    /**
-     * 選択されたカクテル名を表すプロパティ。
-     */
-    private var _cocktailName = ""
-    /**
-     * データベースヘルパーオブジェクト。
-     */
-    private val _helper = DatabaseHelper(this@MainActivity)
-
+  //メディアプレーヤープロパティ
+    private var _player:MediaPlayer? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // カクテルリスト用ListView(lvCocktail)を取得。
-        val lvCocktail = findViewById<ListView>(R.id.lvCocktail)
-        // lvCocktailにリスナを登録。
-        lvCocktail.onItemClickListener = ListItemClickListener()
-    }
-
-    override fun onDestroy() {
-        // ヘルパーオブジェクトの解放。
-        _helper.close()
-        super.onDestroy()
-    }
-
-    /**
-     * 保存ボタンがタップされた時の処理メソッド。
-     */
-    fun onSaveButtonClick(view: View) {
-        // 感想欄を取得。
-        val etNote = findViewById<EditText>(R.id.etNote)
-        // 入力された感想を取得。
-        val note = etNote.text.toString()
-
-        // データベースヘルパーオブジェクトからデータベース接続オブジェクトを取得。
-        val db = _helper.writableDatabase
-
-        // まず、リストで選択されたカクテルのメモデータを削除。その後インサートを行う。
-        // 削除用SQL文字列を用意。
-        val sqlDelete = "DELETE FROM cocktailmemos WHERE _id = ?"
-        // SQL文字列を元にプリペアドステートメントを取得。
-        var stmt = db.compileStatement(sqlDelete)
-        // 変数のバイド。
-        stmt.bindLong(1, _cocktailId.toLong())
-        // 削除SQLの実行。
-        stmt.executeUpdateDelete()
-
-        // インサート用SQL文字列の用意。
-        val sqlInsert = "INSERT INTO cocktailmemos (_id, name, note) VALUES (?, ?, ?)"
-        // SQL文字列を元にプリペアドステートメントを取得。
-        stmt = db.compileStatement(sqlInsert)
-        // 変数のバイド。
-        stmt.bindLong(1, _cocktailId.toLong())
-        stmt.bindString(2, _cocktailName)
-        stmt.bindString(3, note)
-        // インサートSQLの実行。
-        stmt.executeInsert()
-
-        // 感想欄の入力値を消去。
-        etNote.setText("")
-        // カクテル名を表示するTextViewを取得。
-        val tvCocktailName = findViewById<TextView>(R.id.tvCocktailName)
-        // カクテル名を「未選択」に変更。
-        tvCocktailName.text = getString(R.string.tv_name)
-        // 保存ボタンを取得。
-        val btnSave = findViewById<Button>(R.id.btnSave)
-        // 保存ボタンをタップできないように変更。
-        btnSave.isEnabled = false
-    }
-
-    /**
-     * リストがタップされたときの処理が記述されたメンバクラス。
-     */
-    private inner class ListItemClickListener : AdapterView.OnItemClickListener {
-        override fun onItemClick(parent: AdapterView<*>, view: View, position: Int, id: Long) {
-            // タップされた行番号をプロパティの主キーIDに代入。
-            _cocktailId = position
-            // タップされた行のデータを取得。これがカクテル名となるので、プロパティに代入。
-            _cocktailName = parent.getItemAtPosition(position) as String
-            // カクテル名を表示するTextViewを取得。
-            val tvCocktailName = findViewById<TextView>(R.id.tvCocktailName)
-            // カクテル名を表示するTextViewに表示カクテル名を設定。
-            tvCocktailName.text = _cocktailName
-            // 保存ボタンを取得。
-            val btnSave = findViewById<Button>(R.id.btnSave)
-            // 保存ボタンをタップできるように設定。
-            btnSave.isEnabled = true
-
-            // データベースヘルパーオブジェクトからデータベース接続オブジェクトを取得。
-            val db = _helper.writableDatabase
-            // 主キーによる検索SQL文字列の用意。
-            val sql = "SELECT * FROM cocktailmemos WHERE _id = ${_cocktailId}"
-            // SQLの実行。
-            val cursor = db.rawQuery(sql, null)
-
-            // 以下はバインド変数を使う場合の記述。
-//			val sql = "SELECT * FROM cocktailmemos WHERE _id = ?"
-//			val params = arrayOf(_cocktailId.toString())
-//			val cursor = db.rawQuery(sql, params)
-
-            // 以下はSQL文を使わない場合。
-//			val params = arrayOf(_cocktailId.toString())
-//			val cursor = db.query("cocktailmemos", null, "_id = ?", params, null, null, null)
-
-            // データベースから取得した値を格納する変数の用意。データがなかった時のための初期値も用意。
-            var note = ""
-            // SQL実行の戻り値であるカーソルオブジェクトをループさせてデータベース内のデータを取得。
-            while(cursor.moveToNext()) {
-                // カラムのインデックス値を取得。
-                val idxNote = cursor.getColumnIndex("note")
-                // カラムのインデックス値を元に実際のデータを取得。
-                note = cursor.getString(idxNote)
-            }
-            // 感想のEditTextの各画面部品を取得しデータベースの値を反映。
-            val etNote = findViewById<EditText>(R.id.etNote)
-            etNote.setText(note)
+        val loopSwitch = findViewById<SwitchMaterial>(R.id.swLoop)
+        loopSwitch.setOnCheckedChangeListener(LoopSwitchChangedListener())
+        //プロパティのメディアプレーヤーオブジェクトを生成。
+        _player = MediaPlayer()
+        //音声ファイルのURI文字列を作成。
+        val mediaFileUriStr = "android.resource://${packageName}/${R.raw.town_douro}"
+        //音声ファイルのURI文字列を元にURIオブジェクトを生成。
+        val mediaFileUri = Uri.parse(mediaFileUriStr)
+        // プロパティのプレーヤーがnullでなければ…
+        _player?.let {
+            // メディアプレーヤーに音声ファイルを指定。
+            it.setDataSource(this@MainActivity, mediaFileUri)
+            // 非同期でのメディア再生準備が完了した際のリスナを設定。
+            it.setOnPreparedListener(PlayerPreparedListener())
+            // メディア再生が終了した際のリスナを設定。
+            it.setOnCompletionListener(PlayerCompletionListener())
+            // 非同期でメディア再生を準備。
+            it.prepareAsync()
         }
     }
+
+
+        private inner class PlayerPreparedListener:MediaPlayer.OnPreparedListener{
+            override fun onPrepared(mp: MediaPlayer?) {
+                //各ボタンをタップ可能に設定。
+                val btPlay = findViewById<Button>(R.id.btPlay)
+                btPlay.isEnabled = true
+                val btBack = findViewById<Button>(R.id.btBack)
+                btBack.isEnabled = true
+                val btForward = findViewById<Button>(R.id.btForward)
+                btForward.isEnabled = true
+            }
+
+        }
+
+    //再生が終了したときのリスナクラス。
+    private inner class PlayerCompletionListener : MediaPlayer.OnCompletionListener {
+        override fun onCompletion(mp: MediaPlayer) {
+            _player?.let {
+                if (!it.isLooping){
+                    //再生ボタンのラベルを「再生」に設定。
+                    val btPlay = findViewById<Button>(R.id.btPlay)
+                    btPlay.setText(R.string.bt_play_play)
+                }
+            }
+
+        }
+    }
+
+    private inner class LoopSwitchChangedListener : CompoundButton.OnCheckedChangeListener {
+        override fun onCheckedChanged(buttonView: CompoundButton, isChecked: Boolean) {
+            //ループするかどうかを設定。
+            _player?.isLooping = isChecked
+        }
+    }
+
+    fun onPlayButtonClick(view: View){
+        _player?.let{
+            val btPlay = findViewById<Button>(R.id.btPlay)
+            if(it.isPlaying) {
+                //プレーヤーを一時停止。
+                it.pause()
+                //再生ボタンのラベルを「再生」に設定。
+                btPlay.setText(R.string.bt_play_play)
+            }
+            //プレーヤーが再生中でなければ…
+            else {
+                //プレーヤーを再生。
+                it.start()
+                //再生ボタンのラベルを「一時停止」に設定。
+                btPlay.setText(R.string.bt_play_pause)
+            }
+        }
+
+    }
+
+    fun onBackButtonClick(view:View){
+        _player?.seekTo(0)
+    }
+
+    fun onForwardButtonClick(view: View){
+        _player?.let {
+            val duration = it.duration
+            it.seekTo(duration)
+
+            if (!it.isPlaying){
+                val btPlay = findViewById<Button>(R.id.btPlay)
+                btPlay.setText(R.string.bt_play_pause)
+
+                it.start()
+            }
+        }
+    }
+
+
+    override fun onStop() {
+        // プロパティのプレーヤーがnullじゃなかったら
+        _player?.let {
+            // プレーヤーが再生中なら…
+            if (it.isPlaying) {
+                // プレーヤーを停止。
+                it.stop()
+            }
+            // プレーヤーを解放。
+            it.release()
+        }
+        super.onStop()
+    }
+
+
 }
